fde5bf85045591721f03be3d4c37a7e40d3b76f7
shinyakato86 <fujiwarapoteto@gmail.com>
2020-03-13 21:18:51 +0900

    mainvisual修正
    mainvisualの画像パス修正

--- public/js/app.js
+++ public/js/app.js
@@ -49661,8 +49661,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/MAMP/htdocs/lunchmap/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/lunchmap/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/mureview/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/mureview/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
